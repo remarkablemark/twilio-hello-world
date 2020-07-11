3264b777fa682c37ecc90e53ae0a107e93e0cb02
Mark <mark@remarkablemark.org>
2020-07-10 23:58:39 -0400

    refactor(index): replace console.log and console.error with debug

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 
+const twilio = require('twilio');
+const { name } = require('./package');
+const debug = require('debug')(name);
+
 const {
   TWILIO_ACCOUNT_SID,
   TWILIO_AUTH_TOKEN,
@@ -7,12 +11,10 @@ const {
   TO_PHONE_NUMBER,
 } = process.env;
 
-console.log('Account SID:', TWILIO_ACCOUNT_SID);
-console.log('Auth Token:', Array(TWILIO_AUTH_TOKEN.length).join('*'));
-console.log('From:', FROM_PHONE_NUMBER);
-console.log('To:', TO_PHONE_NUMBER);
-
-const twilio = require('twilio');
+debug('Account SID:', TWILIO_ACCOUNT_SID);
+debug('Auth Token:', Array(TWILIO_AUTH_TOKEN.length).join('*'));
+debug('From:', FROM_PHONE_NUMBER);
+debug('To:', TO_PHONE_NUMBER);
 
 const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
@@ -23,8 +25,8 @@ const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
       from: FROM_PHONE_NUMBER,
       to: TO_PHONE_NUMBER,
     });
-    console.log(message);
+    debug(message);
   } catch (error) {
-    console.error(error);
+    debug(error);
   }
 })();
